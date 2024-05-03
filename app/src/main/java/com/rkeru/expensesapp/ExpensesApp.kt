e7fb464783d33f8d0947f207d2a7de0bec95dbab
keru <roberto.cherubin@yahoo.it>
2024-05-03 10:49:13 +0200

    Minor Fix

--- app/src/main/java/com/rkeru/expensesapp/ExpensesApp.kt
+++ app/src/main/java/com/rkeru/expensesapp/ExpensesApp.kt
@@ -1,7 +1,7 @@
 package com.rkeru.expensesapp
 
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -42,7 +42,7 @@ fun ExpensesTopAppBar(
             if (canNavigateBack) {
                 IconButton(onClick = navigateUp) {
                     Icon(
-                       imageVector = Icons.Filled.ArrowBack,
+                       imageVector = Icons.AutoMirrored.Filled.ArrowBack,
                         contentDescription = "back"  // TODO add to string res
                     )
                 }
